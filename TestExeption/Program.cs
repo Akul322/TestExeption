5bd027ab689819cc7a72400adde104a98a571f88
Akul <artiluckashov0000@gmail.com>
2023-11-14 13:03:24 +0300

    Добавьте файлы проекта.

---
+++ TestExeption/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class MyException : Exception
+    {
+        public MyException()
+        { }
+        public MyException(string message)
+            : base(message)
+        { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberReader numberReader = new NumberReader();
+            numberReader.NumberEnteredEvent += ShowNumber;
+            try
+            {
+                numberReader.Read();
+            }
+            catch (MyException)
+            {
+                Console.WriteLine("Writed incorret value");
+            }
+            Console.ReadLine();
+        }
+        static void ShowNumber(int number)
+        {
+            List<string> ls = new List<string>();
+
+            ls.Add("Негрич");
+            ls.Add("Бобрик");
+            ls.Add("Акулибаба");
+            ls.Add("Чупилко");
+            ls.Add("Хренов");
+
+            switch (number)
+            {
+                case 1:
+                    {
+                        Console.WriteLine();
+                        ls.Sort();
+
+                        foreach (string lis in ls)
+                        {
+                            Console.WriteLine(lis);
+                        }
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.WriteLine();
+                        ls.Sort();
+                        ls.Reverse();
+
+                        foreach (string lis in ls)
+                        {
+                            Console.WriteLine(lis);
+                        }
+                    }
+                    break;
+            }
+
+        }
+        class NumberReader
+        {
+            public delegate void EnteredNumberDel(int number);
+            public event EnteredNumberDel NumberEnteredEvent;
+
+
+            public void Read()
+            {
+                Console.WriteLine("Need write value 1 or 2");
+
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number != 1 && number != 2) throw new MyException();
+
+                NumberEntered(number);
+            }
+            protected virtual void NumberEntered(int number)
+            {
+                NumberEnteredEvent?.Invoke(number);
+            }
+        }
+    }
+}
---
+++ TestExeption/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("TestExeption")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestExeption")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ac330f54-5df8-4889-ad0b-e0037a8bd057")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
